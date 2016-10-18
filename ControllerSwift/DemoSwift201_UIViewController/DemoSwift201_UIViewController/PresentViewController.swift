b27fd14ce5fd0094f6bc2e95779a93ae5d6f93e3
herman <zhangsy757@163.com>
2016-10-18 16:28:32 +0800

    完善viewController方法

--- ControllerSwift/DemoSwift201_UIViewController/DemoSwift201_UIViewController/PresentViewController.swift
+++ ControllerSwift/DemoSwift201_UIViewController/DemoSwift201_UIViewController/PresentViewController.swift
@@ -16,13 +16,26 @@ class PresentViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         // 标题
-        self.navigationItem.title = "PresentViewController"
+        // 方法1 如果使用了tabbarController时，使用此方法可以避免tabbar切换按钮标题与导航栏标题显示异常
+//        self.navigationItem.title = "PresentViewController"
+        // 方法2
+//        self.title = "PresentViewController"
+        // 方法3
+//        self.setNavigationTitle("PresentViewController", titleFont: UIFont.boldSystemFontOfSize(20.0), titleColor: UIColor.redColor())
+//        self.setNavigationDefaultFontTitle("PresentViewController")
+        let label = UILabel(frame: CGRectMake(0.0, 0.0, 200.0, 30.0))
+        label.text = "PresentViewController"
+        label.textAlignment = NSTextAlignment.Center
+        label.textColor = UIColor.greenColor()
+        label.font = UIFont.systemFontOfSize(10.0)
+        self.setNavigationTitleView(label)
         
         // 视图自适度
         self.autoSize()
         
-        // 返回前一个视图控制器
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "back", style: UIBarButtonItemStyle.Done, target: self, action: Selector("backClick"))
+        // 返回前一个视图控制器，导航栏左按钮
+//        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "back", style: UIBarButtonItemStyle.Done, target: self, action: Selector("backClick"))
+        self.navigationItem.leftBarButtonItem = self.setNavigationBarButton("取消", normalColor: UIColor.blackColor(), highlightedColor: UIColor.redColor(), normalImage: UIImage(named: "backImage")!, highlightedImage: UIImage(named: "backImage")!, action: Selector("backClick"), leftButton: true)
     }
 
     override func didReceiveMemoryWarning() {
--- ControllerSwift/DemoSwift201_UIViewController/DemoSwift201_UIViewController/UIViewController+Extension.swift
+++ ControllerSwift/DemoSwift201_UIViewController/DemoSwift201_UIViewController/UIViewController+Extension.swift
@@ -75,5 +75,116 @@ extension UIViewController
             }
         }
     }
+    
+    
+    // MARK: - 导航栏标题设置
+    
+    /// 设置导航栏标题视图
+    func setNavigationTitleView(titleView:UIView)
+    {
+        if let viewTmp:UIView = titleView
+        {
+            let view = UIView(frame: CGRectMake(0.0, 0.0, CGRectGetWidth(titleView.frame), CGRectGetHeight(titleView.frame)))
+            view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleLeftMargin, UIViewAutoresizing.FlexibleRightMargin]
+            
+            view.addSubview(viewTmp)
+            viewTmp.autoresizesSubviews = true;
+            viewTmp.center = view.center;
+            
+            self.navigationItem.titleView = view;
+        }
+    }
+    
+    /// 设置导航栏标题
+    func setNavigationTitle(title:String, titleFont:UIFont, titleColor:UIColor)
+    {
+        // 系统方法 张绍裕 20150604
+        self.navigationItem.title = title;
+        self.navigationController!.navigationBar.titleTextAttributes = [NSFontAttributeName:titleFont, NSForegroundColorAttributeName:titleColor]
+        
+        // 自定义label标签方法
+        /*
+        let label = UILabel(frame: CGRectMake(0.0, 0.0, 200.0, CGRectGetHeight(self.navigationBar.frame)))
+        label.font = titleFont
+        label.textColor = titleColor
+        label.textAlignment = NSTextAlignment.Center
+        self.navigationController!.navigationItem.titleView = label;
+        */
+    }
+    
+    /// 设置默认导航栏标题
+    func setNavigationDefaultFontTitle(title:String)
+    {
+        self.setNavigationTitle((title.isEmpty ? "未设置标题" : title), titleFont: UIFont.boldSystemFontOfSize(18.0), titleColor: UIColor.whiteColor())
+    }
+    
+    // MARK: - 导航栏左右按钮
+    func setNavigationBarButton(title:String, normalColor:UIColor, highlightedColor:UIColor, normalImage:UIImage, highlightedImage:UIImage, action:Selector, leftButton:Bool) -> UIBarButtonItem
+    {
+        let buttonFrame:CGRect = CGRectMake(0.0, 0.0, (sizeButton * 3.0), sizeButton)
+        
+        let button = UIButton(frame: buttonFrame)
+        
+        if !leftButton
+        {
+            // 右按钮时向右对齐
+            button.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Right
+        }
+        
+        // 设置按钮标题
+        if !title.isEmpty
+        {
+            var titleButton:String = title
+            // titleButton = "返回"
+            
+            /*
+            if 1 == self.viewControllers.indexOf(self)
+            {
+            // 始终设置返回按钮的标题为第一个视图控制器名称
+            let previousVC = self.viewControllers.first
+            titleButton = previousVC!.navigationItem.title!;
+            }
+            */
+            
+            button.titleEdgeInsets = UIEdgeInsetsMake(0.0, -15.0, 0.0, 0.0);
+            button.titleLabel!.font = UIFont.boldSystemFontOfSize(14.0)
+            
+            button.setTitle(titleButton, forState: UIControlState.Normal)
+            if let normalColorTmp:UIColor = normalColor
+            {
+                button.setTitleColor(normalColorTmp, forState: UIControlState.Normal)
+            }
+            if let highlightedColorTmp:UIColor = highlightedColor
+            {
+                button.setTitleColor(highlightedColorTmp, forState: UIControlState.Highlighted)
+            }
+        }
+        
+        // 设置按钮图标
+        button.imageEdgeInsets = UIEdgeInsetsMake(0.0, (!title.isEmpty ? -15.0 : -30.0), 0.0, 0.0);
+        if let normalImageTmp:UIImage = normalImage
+        {
+            button.imageView!.contentMode = UIViewContentMode.ScaleAspectFit
+            button.setImage(normalImageTmp, forState: UIControlState.Normal)
+        }
+        if let highlightedImageTmp:UIImage = highlightedImage
+        {
+            button.imageView!.contentMode = UIViewContentMode.ScaleAspectFit
+            button.setImage(highlightedImageTmp, forState: UIControlState.Highlighted)
+        }
+        
+        // 设置响应事件
+        if let actionTmp:Selector = action
+        {
+            button.addTarget(self, action: actionTmp, forControlEvents: UIControlEvents.TouchUpInside)
+        }
+        
+        button.accessibilityLabel = "back"
+        
+        // 自定义返回按钮
+        let item = UIBarButtonItem(customView: button)
+        return item;
+    }
+
 }
 
